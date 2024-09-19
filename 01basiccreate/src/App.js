77f58089da16280b596bf93e9a141f6c0b976d12
tomarcodinglife <tomarcodinglife@gmail.com>
2024-09-19 08:26:42 +0530

    updated

--- 01basiccreate/src/App.js
+++ 01basiccreate/src/App.js
@@ -1,6 +1,14 @@
+import MyFunction from "./new";
+
+
 function App() {
   return (
-    <h1>Sujit Kumar Singh</h1>
+    // <h1>Sujit Kumar Singh</h1>
+    // <></> its call fregment
+    <> 
+      <MyFunction/>
+      <h1>Sujit Kumar Singh</h1>
+    </>
   );
 }
 
---
+++ 01basiccreate/src/new.jsx
@@ -0,0 +1,8 @@
+
+function MyFunction () {
+    return (
+    <h3>Sujit Kumar Singh | Create with Basic React</h3>
+)
+}
+
+export default MyFunction
