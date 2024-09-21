ae176d6e90aec2e44b2d09b5aa3c19c99d7118df
tomarcodinglife <tomarcodinglife@gmail.com>
2024-09-21 08:10:19 +0530

    updated

---
+++ 04_sujitreact/sujitreact.js
@@ -0,0 +1,26 @@
+function newElementMethod (ElementLibrary, container) {
+    newElement = document.createElement(ElementLibrary.tagName)
+    newElement.innerHTML = ElementLibrary.tagInnerHtml
+    for (const properties in ElementLibrary.tagAttribute) {
+        if (properties==="Children") continue; 
+            newElement.setAttribute(properties, ElementLibrary.tagAttribute[properties])
+    }
+    container.appendChild(newElement)
+}
+
+
+const newElementLibrary = {
+    tagName : "a",
+    tagInnerHtml : "Click Me",
+    tagAttribute : {
+        href : "http://google.com",
+        target : "_blank"
+    }
+}
+
+const div = document.querySelector("#root")
+
+
+newElementMethod(newElementLibrary, div)
+
+
