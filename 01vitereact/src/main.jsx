8e9a587f94a66ed70d8908820061b1fd927ee477
tomarcodinglife <tomarcodinglife@gmail.com>
2024-09-28 07:13:31 +0530

    updated

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import React from 'react'
 import App from './App.jsx'
+import ReactDom from 'react-dom/client'
+import {jsx as _jsx} from "react/jsx-runtime"
 
 function MyApp () {
   return (
