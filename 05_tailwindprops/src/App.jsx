5a3d6176ef11e8414d11fbdf82642e105e3325aa
tomarcodinglife <tomarcodinglife@gmail.com>
2024-09-29 17:01:19 +0530

    updated

---
+++ 05_tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-yellow-500 p-5 bg-red-800 rounded-xl'>Sujit Kumar Singh Coding React with Tailwind</h1>
+    </>
+  )
+}
+
+export default App
