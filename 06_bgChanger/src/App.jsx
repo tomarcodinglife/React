c8b8d647e99ef2a2356c04ac8136ce3ae53e77dd
tomarcodinglife <tomarcodinglife@gmail.com>
2024-10-27 18:26:33 +0530

    updated

--- 06_bgChanger/src/App.jsx
+++ 06_bgChanger/src/App.jsx
@@ -9,15 +9,19 @@ function App() {
       style={{backgroundColor: color}}>
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
 
-              <div className="flex flex-wrap justify-center gap-2 shadow-lg bg-black rounded-xl p-1">
+          <div className="flex flex-wrap justify-center gap-2 shadow-lg bg-orange-600 p-2 rounded-lg">
                 
-                <button className="w-24 h-8 outline-none p-4 border-2 border-white" style={{backgroundColor: ("blue")}}>Blue</button>
-                <button className="w-24 h-8 outline-none p-4 border-2 border-white" style={{backgroundColor: ("green")}}>Green</button>
-                <button className="w-24 h-8 outline-none p-4 border-2 border-white" style={{backgroundColor: ("olive")}}>Olive</button>
-                <button className="w-24 h-8 outline-none p-4 border-2 border-white" style={{backgroundColor: ("pink")}}>Pink</button>
-                <button className="w-24 h-8 outline-none p-4 border-2 border-white" style={{backgroundColor: ("indigo")}}>Indigo</button>
-              
-              </div>
+            <button onClick={() => setColor("blue")} className="w-24 h-8 p-1 outline-none border-2 border-white hover:border-none" style={{backgroundColor: ("blue")}}>Blue</button>
+
+            <button onClick={()=> setColor("green")} className="w-24 h-8 p-1 outline-none border-2 border-white hover:border-none" style={{backgroundColor: ("green")}}>Green</button>
+
+            <button onClick={()=> setColor("Olive") } className="w-24 h-8 p-1 outline-none border-2 border-white hover:border-none" style={{backgroundColor: ("olive")}}>Olive</button>
+
+            <button onClick={()=> setColor("purple")} className="w-24 h-8 p-1 outline-none border-2 border-white hover:border-none" style={{backgroundColor: ("purple")}}>Purple</button>
+
+            <button onClick={()=> setColor("indigo")} className="w-24 h-8 p-1 outline-none border-2 border-white hover:border-none" style={{backgroundColor: ("indigo")}}>Indigo</button>
+                
+          </div>
 
         </div>
       </div>
