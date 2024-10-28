77794e38e240cdb08cbf451b1647719202a7df0c
tomarcodinglife <tomarcodinglife@gmail.com>
2024-10-28 08:56:33 +0530

    updated

--- 06_passwordGenerator/src/App.jsx
+++ 06_passwordGenerator/src/App.jsx
@@ -4,11 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
   return (
     <>
-      
+        <div>
+
+          <h1 className='text-4xl uppercase font-semibold'>Password Generator</h1>
+          <input type="text" />
+
+        </div>
     </>
   )
 }
