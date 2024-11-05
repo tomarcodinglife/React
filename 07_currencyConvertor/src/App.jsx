a38026a9917c52a5cf98ca9a20fe64ac55de40c6
tomarcodinglife <tomarcodinglife@gmail.com>
2024-11-05 07:38:36 +0530

    updated

---
+++ 07_currencyConvertor/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-slate-900 text-amber-400'>Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App
