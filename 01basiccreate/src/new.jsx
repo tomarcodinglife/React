cbb51234610ebe000d81617b3457f891addeff76
tomarcodinglife <tomarcodinglife@gmail.com>
2025-05-04 13:48:21 +0530

    updated

--- 01basiccreate/src/new.jsx
+++
@@ -1,8 +0,0 @@
-
-function MyFunction () {
-    return (
-    <h3>Sujit Kumar Singh | Create with Basic React</h3>
-)
-}
-
-export default MyFunction
--- 01vitereact/src/new.jsx
+++
@@ -1,9 +0,0 @@
-function MyFunction () {
-    return (
-        <h3>Sujit Tomar | New File With Vite & Tomar</h3>
-    )
-
-}
- 
-
-export default MyFunction
