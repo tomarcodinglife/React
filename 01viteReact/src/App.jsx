e0585dd2090e934d8b4b6a532cc4b9224b12252f
tomarcodinglife <tomarcodinglife@gmail.com>
2025-05-06 08:00:10 +0530

    updated

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,7 +1,17 @@
+import Sujit from "./sujit"
+import Tomar from "./tomar"
+
+
 function App() {
 
+    const username = "Sujit Tomar"
+
   return (
-    <h1>Sujit Tomar</h1>
+    <>
+      <h1>Hi I am {username}</h1>  {/*  here username called expression */}
+      <Sujit/>
+      <Tomar/>
+    </>
   )
 }
 
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,14 +1,30 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Sujit from './sujit.jsx'
-import Tomar from './tomar.jsx'
+
+
+const elementFirst = (
+  <a href="http://google.com" target='blank'> Google </a>
+)
+
+
+const elementSecond = React.createElement(
+  'a',
+  { href:'http://yahoo.com', target:`_blank`},
+  'Yahoo'
+)
 
 createRoot(document.getElementById('root')).render(
-  <>
+  <> 
     <App />
     <Sujit/>
     <Tomar/>
+    {elementFirst}
+    {elementSecond}
     <h1>I am Learnign JSX or React</h1>
   </>
 )
+
+// createRoot(document.getElementById('root')).render(
+//     elementSecond
+// )
