a8aa06cbcd5c7ff809a61560375dcfe156c4fab3
tomarcodinglife <tomarcodinglife@gmail.com>
2025-05-08 12:52:33 +0530

    updated

--- 04projects/03props/src/App.jsx
+++ 04projects/03props/src/App.jsx
@@ -5,16 +5,18 @@ import './App.css'
 import Card from './components/card.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <h1 className='text-amber-400'>Sujit Kumar Singh</h1>
       </div>
-      <Card />
-      <Card />
-      <Card />
+      <div className='flex justify-center items-center m-5'>
+        <Card card_profile_Name = "Sujit Tomar" card_work_type = "Software Engineer" />
+        <Card card_profile_Name = "Rohit Singh" card_work_type = "UI UX Engineer" />
+        <Card card_profile_Name = "Aman Kumar" card_work_type = "DevOps Engineer" />
+      </div>
     </>
   )
 }
