f0740746ae9597704c9f182aa93ed5b680544053
tomarcodinglife <tomarcodinglife@gmail.com>
2025-05-26 08:43:59 +0530

    updated

--- 04projects/06colorChanger/src/App.jsx
+++ 04projects/06colorChanger/src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { useState } from 'react';
+import './index.css' 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('black');
 
   return (
-    <>
-      <div>
-        <h1 className='bg-white'>Sujit Tomar</h1>
-      </div>
-    </>
-  )
+    <div className="h-screen w-full duration-300 m-0 p-0 bg-yellow-400" style={{ backgroundColor: color }}>
+        <div className="fixed bottom-10 flex flex-row gap-4 bg-white rounded-lg p-1 h-12 w-full justify-center items-center">
+          
+           <button className="bg-red-700 p-4 rounded-lg p-4 w-15 h-10 text-white"  onClick={()=>{setColor('red')}}>Red</button>
+           <button className="bg-cyan-700 p-4 rounded-lg p-4 w-15 h-10 text-white"  onClick={()=>{setColor('cyan')}}>Cyan</button>
+           <button className="bg-blue-700 p-4 rounded-lg p-4 w-15 h-10 text-white"  onClick={()=>{setColor('blue')}}>Blue</button>
+           <button className="bg-gray-700 p-4 rounded-lg p-4 w-15 h-10 text-white"  onClick={()=>{setColor('black')}}>Black</button>
+           <button className="bg-yellow-600 p-4 rounded-lg p-4 w-15 h-10 text-white"  onClick={()=>{setColor('yellow')}}>yellow</button>
+           <button className="bg-green-700 p-4 rounded-lg p-4 w-15 h-10 text-white"  onClick={()=>{setColor('green')}}>Green</button>
+
+        </div>
+    </div>
+  );
+
 }
 
 export default App
