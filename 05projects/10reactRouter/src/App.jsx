6c3e78501735f7b0348c3b2924b4336ff8d34060
tomarcodinglife <tomarcodinglife@gmail.com>
2025-06-05 07:57:05 +0530

    updated

---
+++ 05projects/10reactRouter/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div className='h-screen w-screen bg-black'>
+          <h1 className='text-white'>Sujit Kumar Singh</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
