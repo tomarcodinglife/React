96f3a02d05598c652c9eb4e3c6fabacf88c61750
tomarcodinglife <tomarcodinglife@gmail.com>
2025-07-01 08:53:53 +0530

    updated

--- 04importent/05multipleCondition/src/App.jsx
+++ 04importent/05multipleCondition/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{count}</h1>
+      <button onClick={()=>{setCount(count+1)}}>Increace Count</button>
+      <button onClick={()=>{setCount(count-1)}}>Decreace Count</button>
+      {
+        count == 0 ? (<p>Condition 0</p>):
+        count == 1 ? (<p>Condition 1</p>) :
+        count == 2 ? (<p>Condition 2</p>) :
+        count == 3 ? (<p>Condition 3</p>) :
+        count == 4 ? (<p>Condition 4</p>) :
+        count == 5 ? (<p>Condition 5</p>) :
+        (<p>No Condition Available</p>)
+      }
     </>
   )
 }
