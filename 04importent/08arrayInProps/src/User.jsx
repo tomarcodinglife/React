83e4989e3b543403b433a58d093d372febd02419
tomarcodinglife <tomarcodinglife@gmail.com>
2025-07-03 08:30:22 +0530

    updated

---
+++ 04importent/08arrayInProps/src/User.jsx
@@ -0,0 +1,15 @@
+function User({user}){
+ return(
+    <div>
+        <h1>Users Details</h1>
+        {user.map((obj) =>(
+            <div key={item.id} style={{border: "1px solid black", margin: "10px", padding: "10px"}}>
+                <h2>{obj.name}</h2>
+                <p>Age: {obj.age}, Email: {obj.email}, Country: {obj.country}, Mobile: {obj.mobile}, College: {obj.college}</p>
+            </div>
+        ))}
+    </div>
+ )
+}
+
+export default User;
