6664df6db58117c0e9376c8d236966d22b005afc
tomarcodinglife <tomarcodinglife@gmail.com>
2025-07-08 18:13:48 +0530

    updated

---
+++ 04importent/14digitalClock/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Test</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
