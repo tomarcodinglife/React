55753ac684387b84829dc8435b0561308a5829e4
tomarcodinglife <tomarcodinglife@gmail.com>
2025-07-10 09:21:23 +0530

    updated

--- 03hooks/02useEffect/src/App.jsx
+++ 03hooks/02useEffect/src/App.jsx
@@ -1,36 +1,94 @@
-import { useState } from 'react'
-import Addition from './components/Addition'
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [num1, seNum1] = useState(0)
-  const [num2, seNum2] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState("");
 
-  const handleChange1 = (e) => (
-    seNum1(e.target.value) // Update num1 state with the input value
-  )
+// ..........................................................................................................................................
 
-  const handleChange2 = (e) => (
-    seNum2(e.target.value) // Update num2 state with the input value
-  )
+  const namePrint = function namePrint(name) {
+    console.log(`Hello ${name}`);
+  }
 
+  useEffect(() => {
+      namePrint("John");
+  },[]);
+
+// ..........................................................................................................................................
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+// ..........................................................................................................................................
+
+  const counterFunction = function counterFunction() {
+    console.log(`Count is ${count}`);
+  }
+
+  // counterFunction();
+
+  useEffect(() => {
+    counterFunction();
+  }, [count]);
+
+  const currentTime = () => {
+    const date = new Date();
+    return date.toLocaleTimeString();
+  };
+
+  const currentDate = () => {
+    const date = new Date();
+    return date.toLocaleDateString('en-GB');
+  }
+
+  const handleTime = () => {
+    const timeInterval = setInterval(() => {
+      setData(currentTime());
+    }, 1000);
+    return timeInterval;
+  }
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setData(currentTime());
+    }, 1000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+
+  const handleDate = () => {
+    setData(currentDate());
+  }
+    
 
   return (
     <>
-      <h1>Addition Function</h1>
       <div>
-        <label htmlFor="">Number1</label>
-        <input type="number" value={num1} onChange={handleChange1} />
+        <h1>Sujit Kumar</h1>
+        <h2>Count: {count}</h2>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
       </div>
-      <br />
       <div>
-        <label htmlFor="">Number2</label>
-        <input type="number" value={num2} onChange={handleChange2} />
+        <h2>{data}</h2>
+        <button onClick={handleTime}>Current Time</button>
+        <button onClick={handleDate}>Current Date</button>
       </div>
-
-      <br />
-      <Addition a={num1} b={num2} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
