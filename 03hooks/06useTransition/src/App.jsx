f296045a57107e535a494c3b8216e74e606dafff
tomarcodinglife <tomarcodinglife@gmail.com>
2025-07-18 08:51:22 +0530

    update

--- 03hooks/06useTransition/src/App.jsx
+++ 03hooks/06useTransition/src/App.jsx
@@ -1,9 +1,16 @@
 import { useTransition, useState } from 'react'
 
+const msgStyle = {
+  color: 'green',
+  fontSize: '20px',
+  fontWeight: 'bold',
+}
+
 
 function App() {
   // const [pending, setPending] = useState(false)
   const [pending, startTransition] = useTransition();
+  const [message, setMessage] = useState('');  
 
   // const handleButton = () => {
   //   setPending(true)
@@ -18,6 +25,10 @@ function App() {
   //   setPending(false)
   // }
 
+  const messageHandler = () => {
+    setMessage('Sucess')
+  }
+
   const handleButton = () => {
       startTransition(async() => {
         await new Promise((res) => setTimeout(res, 2000));
@@ -25,11 +36,35 @@ function App() {
       })
   }
 
+  const eraseMessage = () => {
+    setMessage('')
+  } 
+
+  const timeSet = () => {
+    setTimeout(() => {
+      messageHandler()
+    }, 2000)
+  }
+
+  const clearInterval = () => {
+    setTimeout(() => {
+      eraseMessage()
+    }, 4000)
+  }
+
+  const handleCaller = () => {
+    timeSet()
+    handleButton()
+    clearInterval(timeSet)
+  }
+
+
   return (
     <>
       <div>
         <h1>useTransition Hook</h1>
-        <button disabled={pending} onClick={handleButton}>Click</button>
+        <button disabled={pending} onClick={handleCaller}>Click</button>
+        <div style={msgStyle}>{message}</div>
       </div>
     </>
   )
