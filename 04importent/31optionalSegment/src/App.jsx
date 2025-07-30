4bfdb02db91ce04cffcd21fd28e48cd20fdda565
tomarcodinglife <tomarcodinglife@gmail.com>
2025-07-30 07:29:24 +0530

    updated

--- 04importent/31optionalSegment/src/App.jsx
+++ 04importent/31optionalSegment/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import Header from './components/Header'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Courses from './pages/Courses'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
 
 
 function App() {
@@ -11,6 +15,10 @@ function App() {
       <Header/>
       <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/courses' element={<Courses/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/login' element={<Login/>}/>
       </Routes>
     </>
   )
--- 04importent/31optionalSegment/src/components/Header.jsx
+++ 04importent/31optionalSegment/src/components/Header.jsx
@@ -1,26 +1,25 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet } from "react-router-dom";
 
-
-function Header () {
+function Header() {
+    return (
     <>
-        return (
-            <div style={{display:'flex', color:'white', backgroundColor:'black'}}>
-            <div>
-                <h1>LOGO</h1>
-            </div>
-            <div>
-                < Link to={'/'} > Home </ Link >
-                < Link to={'/about'} > About </ Link >
-                < Link to={'/courses'} > Course </ Link >
-                < Link to={'/contact'} > Contact </ Link >
-                < Link to={'/login'} > Login </ Link >
+      <div style={{ display: "flex", color: "white", backgroundColor: "black", borderRadius:'2em', alignItems:"center", justifyContent:'space-between'}}>
+        <div style={{paddingLeft:'1em'}}>
+          <h1>LOGO</h1>
+        </div>
+            <div style={{color:'white', paddingRight:'1em'}}>
+            <Link to={"/"} style={{color:'white'}}> Home </Link>
+            <Link to={"/about"} style={{color:'white', marginLeft:'1em'}}> About </Link>
+            <Link to={"/courses"} style={{color:'white', marginLeft:'1em'}}> Course </Link>
+            <Link to={"/contact"} style={{color:'white', marginLeft:'1em'}}> Contact </Link>
+            <Link to={"/login"} style={{color:'white', marginLeft:'1em'}}> Login </Link>
             </div>
         </div>
-        <div>
-            <Outlet/>
-        </div>
-        )
+      <div>
+        <Outlet />
+      </div>
     </>
+    )
 }
 
-export default Header
+export default Header;
