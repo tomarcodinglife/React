08a36888cd25b390e9d08aefb941de72c132cadb
tomarcodinglife <tomarcodinglife@gmail.com>
2025-07-31 08:30:48 +0530

    updated

--- 04importent/32navlinkAndActiveClass/src/components/Header.jsx
+++ 04importent/32navlinkAndActiveClass/src/components/Header.jsx
@@ -4,15 +4,23 @@ import './header.css'
 function Header() {
     return (
     <>
-      <div style={{ display: "flex", color: "white", backgroundColor: "black", borderRadius:'2em', alignItems:"center", justifyContent:'space-between'}}>
+      <div style={{ 
+        display: "flex", 
+        color: "white", 
+        backgroundColor: "black", 
+        borderRadius:'2em', 
+        alignItems:"center", 
+        justifyContent:'space-between'
+        }}>
+
         <div style={{paddingLeft:'1em'}}>
           <h1>LOGO</h1>
         </div>
-            <div style={{color:'white', paddingRight:'1em'}}>
-            <NavLink to={"/"} className={Link}> Home </NavLink>
-            <NavLink to={"/about"}> About </NavLink>
-            <NavLink to={"/courses"}> Course </NavLink>
-            <Link to={"/contact"} className={({isActive}) => 'isActive' ? "Active" : "Inactive"}> Contact </Link>
+            <div style={{paddingRight:'1em'}}>
+            <NavLink to={"/"} className={({ isActive }) => isActive? "Link Active" : "Link"}> Home </NavLink>
+            <NavLink to={"/about"} className={({ isActive }) => isActive? "Link Active" : "Link"}> About </NavLink>
+            <NavLink to={"/courses"} className={({ isActive }) => isActive? "Link Active" : "Link"} > Course </NavLink>
+            <NavLink to={"/contact"} className={({ isActive }) => isActive? "Link Active" : "Link"}> Contact </NavLink>
             </div>
         </div>
       <div>
