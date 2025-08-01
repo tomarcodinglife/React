cecaaa51b1d6010367c93ac35064e93bcee0fadb
tomarcodinglife <tomarcodinglife@gmail.com>
2025-08-01 18:14:17 +0530

    updated

--- 04importent/34jsonServerAndLoader/src/App.jsx
+++ 04importent/34jsonServerAndLoader/src/App.jsx
@@ -1,13 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData, setuserData] = useState([])
+  const [loding, setLoding] = useState(false)
+
+  useEffect(()=>{
+    setLoding(true)
+    getUserData()
+  }, [])
+
+
+  const getUserData = async() => {
+    const url = 'http://localhost:3000/users'
+    let response = await fetch(url);
+    response = await response.json();
+    // console.log(response)
+    setuserData(response)
+    setLoding(false)
+  }
+
+  console.log("userdata", userData)
 
   return (
     <>
-      <h1>Test</h1>
+      <h1>Users Data</h1>
+        <table>
+          <thead>
+            <tr>
+              <th> ID </th>
+              <th> Name </th>
+              <th> Age </th>
+              <th> Gender </th>
+              <th> Type </th>
+              <th> Location </th>
+              <th> Mobile </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              !loding?
+              userData.map((user)=> (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.age}</td>
+                  <td>{user.gender}</td>
+                  <td>{user.usertype}</td>
+                  <td>{user.location.city}</td>
+                  <td>{user.mobile}</td>
+                </tr>
+              ))
+              : <h2>Loding..</h2>
+            }
+          </tbody>
+        </table>
     </>
   )
 }
---
+++ 04importent/35apiPostMethod/src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Heder () {
+
+    return (
+        <>
+            <div>
+                <h1>LOGO</h1>
+            </div>
+            <div>
+                
+            </div>
+        </>
+    )
+}
