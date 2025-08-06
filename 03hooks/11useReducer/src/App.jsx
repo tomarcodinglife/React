edfc1e74e8dfbd386fe56775147dc262769992f6
tomarcodinglife <tomarcodinglife@gmail.com>
2025-08-06 17:45:09 +0530

    updated

--- 03hooks/11useReducer/src/App.jsx
+++ 03hooks/11useReducer/src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
+
+
+  const emptyData = {
+    name:"",
+    password:"",
+    email:"",
+    city:"",
+    address : "",
+  }
+
+  const reducer = (data, action) => {
+    return {...data, [action.type] : action.val}
+    // console.log(data, action)
+  }
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, dispatch] = useReducer(reducer, emptyData)
+  console.log(data)
+
 
   return (
     <>
       <h1>useReducer</h1>
+      <input type="text" onChange={(e)=>dispatch({val : e.target.value, type:'name'})}  placeholder='Enter Name'/>
+      <br /><br />
+      <input type="text" onChange={(e) => dispatch({val : e.target.value, type:'password'})}  placeholder='Enter Password'/>
+      <br /><br />
+      <input type="text"  onChange={(e) =>dispatch({val : e.target.value, type:'email'})} placeholder='Enter Email'/>
+      <br /><br />
+      <input type="text"  onChange={(e)=> dispatch({val : e.target.value, type:'city'}) } placeholder='Enter City'/>
+      <br /><br />
+      <input type="text" onChange={(e) => dispatch({val:e.target.value, type:'address'})}  placeholder='Enter Address'/>
+      <br /><br />
+      <button>Add Details</button>
     </>
   )
 }
